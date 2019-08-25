e44e8f8f73fdc33702f9ab99b0554a3fb1d23dab
Osmar CS <osmar.costa.s@gmail.com>
2019-08-25 01:14:46 -0300

    :bug: fix only set password_hash when pass prop password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -16,6 +16,9 @@ class User extends Model {
       }
     );
     this.addHook('beforeSave', async(user) => {
+      if(!user.password) {
+        return;
+      }
       user.password_hash = await bcrypt.hash(user.password, 8);
     });
     return this;
