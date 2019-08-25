4f00266261a60916f248e58098d781dc176fcff0
Osmar CS <osmar.costa.s@gmail.com>
2019-08-25 00:10:16 -0300

    :sparkles: add jwt, create sessionController to generate session token

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+import { secret, expiresIn } from '../../config/auth';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      res.status(401).json({ error: 'user not found' });
+    }
+    if (!(await user.checkPassword(password))) {
+      res.status(401).json({ error: 'password incorrent' });
+    }
+
+    const { id, name } = user;
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, secret, {
+        expiresIn: expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
---
+++ src/config/auth.js
@@ -0,0 +1,2 @@
+export const secret = 'ecc24ef61790bb3d271615842ecd0c70';
+export const expiresIn = '7d';
