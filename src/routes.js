2dd299e48e3b0f6942b308709907d5b0c848f42d
Osmar CS <osmar.costa.s@gmail.com>
2019-08-30 00:35:34 -0300

    :sparkles: add upload de avatares by multer

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import AuthMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
+const upload = multer(multerConfig);
 const routes = new Router();
 
 routes.post('/users', UserController.store);
@@ -11,5 +14,8 @@ routes.post('/session', SessionController.store);
 routes.use(AuthMiddleware);
 routes.put('/users', UserController.update);
 
+routes.post('/files', upload.single('file'), (req, res) =>
+  res.json({ ok: true })
+);
 
 export default routes;
