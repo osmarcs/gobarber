763de41d1053071343fddcee922f78dd86e5509f
Osmar CS <osmar.costa.s@gmail.com>
2019-08-30 20:55:58 -0300

    :sparkles: add migration, model and controller to Files

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import AuthMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
@@ -14,8 +15,6 @@ routes.post('/session', SessionController.store);
 routes.use(AuthMiddleware);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) =>
-  res.json({ ok: true })
-);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
