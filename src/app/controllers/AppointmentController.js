4fefd2893c01391cb6227de48098dc1f74e8cc9e
Osmar CS <osmar.costa.s@gmail.com>
2019-08-30 23:37:49 -0300

    :sparkles: add validation for paste dates and available date in appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -27,6 +28,26 @@ class AppointmentController {
     if (!isProvider) {
       return res.status(400).json({
         errors: 'You can only create appointments with provider',
+      });
+    }
+
+    const hourStart = startOfHour(parseISO(date));
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({
+        error: 'Past dates are not permitted',
+      });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+    if (checkAvailability) {
+      return res.status(400).json({
+        error: 'Appointment date is not available',
       });
     }
 
