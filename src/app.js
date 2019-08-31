1d17aa9ed4f5434a5ff271adb440e4123d03b8b3
Osmar CS <osmar.costa.s@gmail.com>
2019-08-30 22:04:41 -0300

    :sparkles: add route to recovery all providers
    config models User and File to server avatar
    config app.js to server static files

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path';
 import express from 'express';
 import routes from './routes';
 import './database';
@@ -11,6 +12,10 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import multer from 'multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+
 import AuthMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
@@ -16,5 +19,6 @@ routes.use(AuthMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
 
 export default routes;
