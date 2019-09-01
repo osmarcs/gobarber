e9c00f9a8feaf33aafa374831cdb8c33a99dd7e5
Osmar CS <osmar.costa.s@gmail.com>
2019-09-01 20:41:37 -0300

    :sparkles: add and config queues, add job to send cancelation email

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,7 +5,8 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
@@ -137,23 +138,8 @@ class AppointmentController {
     appointment.canceled_at = new Date();
     await appointment.save();
 
-    const appointmentDate = format(
-      appointment.date,
-      "dd 'de' MMMM', às' H:mm'h'",
-      {
-        locale: pt,
-      }
-    );
-
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento Cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: appointmentDate,
-      },
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
