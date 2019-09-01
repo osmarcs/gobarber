53f2af5a62e08174c0b04ae48a3f61b6003b8055
Osmar CS <osmar.costa.s@gmail.com>
2019-08-31 22:17:40 -0300

    :sparkles: add ScheduleController to provider see appointments

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AuthMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleCotroller from './app/controllers/ScheduleCotroller';
 
 const upload = multer(multerConfig);
 const routes = new Router();
@@ -25,5 +26,7 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleCotroller.index);
 
 export default routes;
