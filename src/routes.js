a1cff02c643d604c4d1b151a99886f12360c4cce
Osmar CS <osmar.costa.s@gmail.com>
2019-09-01 22:49:09 -0300

    :sparkles: add AvailableController to show available hours of provider

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ import multerConfig from './config/multer';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleCotroller from './app/controllers/ScheduleCotroller';
 import NoticationsController from './app/controllers/NoticationsController';
+import AvailableController from './app/controllers/AvailableController';
 
 const upload = multer(multerConfig);
 const routes = new Router();
@@ -24,6 +25,7 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
